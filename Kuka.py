823fe6e19e5bb16a47e87be1da178cda0922cc5f
simon <simon.ing.89@gmail.com>
2021-05-22 15:15:09 +0200

    Done;)

--- Kuka.py
+++ Kuka.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import PySimpleGUI as sg
 
 
 def nothing(x):
@@ -7,93 +8,181 @@ def nothing(x):
 
 
 def init_GUI():
-    cv2.namedWindow("GUI", cv2.WINDOW_NORMAL)
-    cv2.createTrackbar("LH_YELLOW", "GUI", 25, 255, nothing)
-    cv2.createTrackbar("LS_YELLOW", "GUI", 30, 255, nothing)
-    cv2.createTrackbar("LV_YELLOW", "GUI", 100, 255, nothing)
-    cv2.createTrackbar("UH_YELLOW", "GUI", 30, 255, nothing)
-    cv2.createTrackbar("US_YELLOW", "GUI", 180, 255, nothing)
-    cv2.createTrackbar("UV_YELLOW", "GUI", 240, 255, nothing)
+    layout = [[sg.Text('YELLOW', text_color='Black', background_color='yellow', font=('helvetica', 15))],
+              [sg.Text('hue'),
+               sg.Slider((0, 179), 0, key='yhl', enable_events=True),
+               sg.Slider((0, 179), 179, key='yhu', enable_events=True),
+               sg.Text('sat'),
+               sg.Slider((0, 255), 0, key='ysl', enable_events=True),
+               sg.Slider((0, 255), 255, key='ysu', enable_events=True),
+               sg.Text('val'),
+               sg.Slider((0, 255), 0, key='yvl', enable_events=True),
+               sg.Slider((0, 255), 255, key='yvu', enable_events=True)],
 
-    cv2.createTrackbar("LH_GREEN", "GUI", 50, 255, nothing)
-    cv2.createTrackbar("LS_GREEN", "GUI", 0, 255, nothing)
-    cv2.createTrackbar("LV_GREEN", "GUI", 0, 255, nothing)
-    cv2.createTrackbar("UH_GREEN", "GUI", 80, 255, nothing)
-    cv2.createTrackbar("US_GREEN", "GUI", 255, 255, nothing)
-    cv2.createTrackbar("UV_GREEN", "GUI", 255, 255, nothing)
+              [sg.Text('GREEN', text_color='Black', background_color='green', font=('helvetica', 15))],
+              [sg.Text('hue'),
+               sg.Slider((0, 179), 0, key='ghl', enable_events=True),
+               sg.Slider((0, 179), 179, key='ghu', enable_events=True),
+               sg.Text('sat'),
+               sg.Slider((0, 255), 0, key='gsl', enable_events=True),
+               sg.Slider((0, 255), 255, key='gsu', enable_events=True),
+               sg.Text('val'),
+               sg.Slider((0, 255), 0, key='gvl', enable_events=True),
+               sg.Slider((0, 255), 255, key='gvu', enable_events=True)],
 
-    cv2.createTrackbar("LH_RED", "GUI", 5, 255, nothing)
-    cv2.createTrackbar("LS_RED", "GUI", 50, 255, nothing)
-    cv2.createTrackbar("LV_RED", "GUI", 140, 255, nothing)
-    cv2.createTrackbar("UH_RED", "GUI", 15, 255, nothing)
-    cv2.createTrackbar("US_RED", "GUI", 215, 255, nothing)
-    cv2.createTrackbar("UV_RED", "GUI", 240, 255, nothing)
+              [sg.Text('RED', text_color='Black', background_color='red', font=('helvetica', 15))],
+              [sg.Text('hue'),
+               sg.Slider((0, 179), 0, key='rhl', enable_events=True),
+               sg.Slider((0, 179), 179, key='rhu', enable_events=True),
+               sg.Text('sat'),
+               sg.Slider((0, 255), 0, key='rsl', enable_events=True),
+               sg.Slider((0, 255), 255, key='rsu', enable_events=True),
+               sg.Text('val'),
+               sg.Slider((0, 255), 0, key='rvl', enable_events=True),
+               sg.Slider((0, 255), 255, key='rvu', enable_events=True)],
 
-    cv2.createTrackbar("LH_BLUE", "GUI", 73, 255, nothing)
-    cv2.createTrackbar("LS_BLUE", "GUI", 0, 255, nothing)
-    cv2.createTrackbar("LV_BLUE", "GUI", 0, 255, nothing)
-    cv2.createTrackbar("UH_BLUE", "GUI", 130, 255, nothing)
-    cv2.createTrackbar("US_BLUE", "GUI", 255, 255, nothing)
-    cv2.createTrackbar("UV_BLUE", "GUI", 255, 255, nothing)
+              [sg.Text('BLUE', text_color='Black', background_color='blue', font=('helvetica', 15))],
+              [sg.Text('hue'),
+               sg.Slider((0, 179), 0, key='bhl', enable_events=True),
+               sg.Slider((0, 179), 179, key='bhu', enable_events=True),
+               sg.Text('sat'),
+               sg.Slider((0, 255), 0, key='bsl', enable_events=True),
+               sg.Slider((0, 255), 255, key='bsu', enable_events=True),
+               sg.Text('val'),
+               sg.Slider((0, 255), 0, key='bvl', enable_events=True),
+               sg.Slider((0, 255), 255, key='bvu', enable_events=True)]
+              ]
+
+    window = sg.Window('Title', layout)
+    return window
+
+# def init_GUI():
+#     cv2.namedWindow("GUI", cv2.WINDOW_NORMAL)
+#     cv2.createTrackbar("LH_YELLOW", "GUI", 25, 255, nothing)
+#     cv2.createTrackbar("LS_YELLOW", "GUI", 30, 255, nothing)
+#     cv2.createTrackbar("LV_YELLOW", "GUI", 100, 255, nothing)
+#     cv2.createTrackbar("UH_YELLOW", "GUI", 30, 255, nothing)
+#     cv2.createTrackbar("US_YELLOW", "GUI", 180, 255, nothing)
+#     cv2.createTrackbar("UV_YELLOW", "GUI", 240, 255, nothing)
+#
+#     cv2.createTrackbar("LH_GREEN", "GUI", 50, 255, nothing)
+#     cv2.createTrackbar("LS_GREEN", "GUI", 0, 255, nothing)
+#     cv2.createTrackbar("LV_GREEN", "GUI", 0, 255, nothing)
+#     cv2.createTrackbar("UH_GREEN", "GUI", 80, 255, nothing)
+#     cv2.createTrackbar("US_GREEN", "GUI", 255, 255, nothing)
+#     cv2.createTrackbar("UV_GREEN", "GUI", 255, 255, nothing)
+#
+#     cv2.createTrackbar("LH_RED", "GUI", 5, 255, nothing)
+#     cv2.createTrackbar("LS_RED", "GUI", 50, 255, nothing)
+#     cv2.createTrackbar("LV_RED", "GUI", 140, 255, nothing)
+#     cv2.createTrackbar("UH_RED", "GUI", 15, 255, nothing)
+#     cv2.createTrackbar("US_RED", "GUI", 215, 255, nothing)
+#     cv2.createTrackbar("UV_RED", "GUI", 240, 255, nothing)
+#
+#     cv2.createTrackbar("LH_BLUE", "GUI", 73, 255, nothing)
+#     cv2.createTrackbar("LS_BLUE", "GUI", 0, 255, nothing)
+#     cv2.createTrackbar("LV_BLUE", "GUI", 0, 255, nothing)
+#     cv2.createTrackbar("UH_BLUE", "GUI", 130, 255, nothing)
+#     cv2.createTrackbar("US_BLUE", "GUI", 255, 255, nothing)
+#     cv2.createTrackbar("UV_BLUE", "GUI", 255, 255, nothing)
 
 
-def calibrate(frame):
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+def calibrate(window):
 
-    l_h_y = cv2.getTrackbarPos("LH_YELLOW", "GUI")
-    l_s_y = cv2.getTrackbarPos("LS_YELLOW", "GUI")
-    l_v_y = cv2.getTrackbarPos("LV_YELLOW", "GUI")
-    u_h_y = cv2.getTrackbarPos("UH_YELLOW", "GUI")
-    u_s_y = cv2.getTrackbarPos("US_YELLOW", "GUI")
-    u_v_y = cv2.getTrackbarPos("UV_YELLOW", "GUI")
-    l_b_y = np.array([l_h_y, l_s_y, l_v_y])
-    u_b_y = np.array([u_h_y, u_s_y, u_v_y])
-    mask_yellow = cv2.inRange(hsv, l_b_y, u_b_y)
+    event, values = window.read()
+
+    yellow_lower = (values['yhl'], values['ysl'], values['yvl'])
+    yellow_upper = (values['yhu'], values['ysu'], values['yvu'])
+
+    green_lower = (values['ghl'], values['gsl'], values['gvl'])
+    green_upper = (values['ghu'], values['gsu'], values['gvu'])
+
+    red_lower = (values['rhl'], values['rsl'], values['rvl'])
+    red_upper = (values['rhu'], values['rsu'], values['rvu'])
+
+    blue_lower = (values['bhl'], values['bsl'], values['bvl'])
+    blue_upper = (values['bhu'], values['bsu'], values['bvu'])
+
+    return yellow_lower, yellow_upper, green_lower, green_upper, red_lower, red_upper, blue_lower, blue_upper
+
+
+def show_cal_screen(hsv, frame, thresholds):
+    mask_yellow = cv2.inRange(hsv, thresholds[0], thresholds[1])
     res_yellow = cv2.bitwise_and(frame, frame, mask=mask_yellow)
     cv2.putText(res_yellow, "Yellow", (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 200), 2)
-
-    l_h_g = cv2.getTrackbarPos("LH_GREEN", "GUI")
-    l_s_g = cv2.getTrackbarPos("LS_GREEN", "GUI")
-    l_v_g = cv2.getTrackbarPos("LV_GREEN", "GUI")
-    u_h_g = cv2.getTrackbarPos("UH_GREEN", "GUI")
-    u_s_g = cv2.getTrackbarPos("US_GREEN", "GUI")
-    u_v_g = cv2.getTrackbarPos("UV_GREEN", "GUI")
-    l_b_g = np.array([l_h_g, l_s_g, l_v_g])
-    u_b_g = np.array([u_h_g, u_s_g, u_v_g])
-    mask_green = cv2.inRange(hsv, l_b_g, u_b_g)
+    mask_green = cv2.inRange(hsv, thresholds[2], thresholds[3])
     res_green = cv2.bitwise_and(frame, frame, mask=mask_green)
     cv2.putText(res_green, "Green", (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-    l_h_r = cv2.getTrackbarPos("LH_RED", "GUI")
-    l_s_r = cv2.getTrackbarPos("LS_RED", "GUI")
-    l_v_r = cv2.getTrackbarPos("LV_RED", "GUI")
-    u_h_r = cv2.getTrackbarPos("UH_RED", "GUI")
-    u_s_r = cv2.getTrackbarPos("US_RED", "GUI")
-    u_v_r = cv2.getTrackbarPos("UV_RED", "GUI")
-    l_b_r = np.array([l_h_r, l_s_r, l_v_r])
-    u_b_r = np.array([u_h_r, u_s_r, u_v_r])
-    mask_red = cv2.inRange(hsv, l_b_r, u_b_r)
+    mask_red = cv2.inRange(hsv, thresholds[4], thresholds[5])
     res_red = cv2.bitwise_and(frame, frame, mask=mask_red)
     cv2.putText(res_red, "Red", (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-
-    l_h_b = cv2.getTrackbarPos("LH_BLUE", "GUI")
-    l_s_b = cv2.getTrackbarPos("LS_BLUE", "GUI")
-    l_v_b = cv2.getTrackbarPos("LV_BLUE", "GUI")
-    u_h_b = cv2.getTrackbarPos("UH_BLUE", "GUI")
-    u_s_b = cv2.getTrackbarPos("US_BLUE", "GUI")
-    u_v_b = cv2.getTrackbarPos("UV_BLUE", "GUI")
-    l_b_b = np.array([l_h_b, l_s_b, l_v_b])
-    u_b_b = np.array([u_h_b, u_s_b, u_v_b])
-    mask_blue = cv2.inRange(hsv, l_b_b, u_b_b)
+    mask_blue = cv2.inRange(hsv, thresholds[6], thresholds[7])
     res_blue = cv2.bitwise_and(frame, frame, mask=mask_blue)
     cv2.putText(res_blue, "Blue", (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-    # cv2.imshow("blue", res_blue)
-
     top = np.hstack((res_yellow, res_green))
     bottom = np.hstack((res_red, res_blue))
     img = np.vstack((top, bottom))
     cv2.imshow("Calibration", cv2.resize(img, (0, 0), fx=0.6, fy=0.6))
-    return l_b_y, u_b_y, l_b_g, u_b_g, l_b_r, u_b_r, l_b_b, u_b_b
+#
+#
+# def calibrate(frame):
+#     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+#
+#     l_h_y = cv2.getTrackbarPos("LH_YELLOW", "GUI")
+#     l_s_y = cv2.getTrackbarPos("LS_YELLOW", "GUI")
+#     l_v_y = cv2.getTrackbarPos("LV_YELLOW", "GUI")
+#     u_h_y = cv2.getTrackbarPos("UH_YELLOW", "GUI")
+#     u_s_y = cv2.getTrackbarPos("US_YELLOW", "GUI")
+#     u_v_y = cv2.getTrackbarPos("UV_YELLOW", "GUI")
+#     l_b_y = np.array([l_h_y, l_s_y, l_v_y])
+#     u_b_y = np.array([u_h_y, u_s_y, u_v_y])
+#     mask_yellow = cv2.inRange(hsv, l_b_y, u_b_y)
+#     res_yellow = cv2.bitwise_and(frame, frame, mask=mask_yellow)
+#     cv2.putText(res_yellow, "Yellow", (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 200), 2)
+#
+#     l_h_g = cv2.getTrackbarPos("LH_GREEN", "GUI")
+#     l_s_g = cv2.getTrackbarPos("LS_GREEN", "GUI")
+#     l_v_g = cv2.getTrackbarPos("LV_GREEN", "GUI")
+#     u_h_g = cv2.getTrackbarPos("UH_GREEN", "GUI")
+#     u_s_g = cv2.getTrackbarPos("US_GREEN", "GUI")
+#     u_v_g = cv2.getTrackbarPos("UV_GREEN", "GUI")
+#     l_b_g = np.array([l_h_g, l_s_g, l_v_g])
+#     u_b_g = np.array([u_h_g, u_s_g, u_v_g])
+#     mask_green = cv2.inRange(hsv, l_b_g, u_b_g)
+#     res_green = cv2.bitwise_and(frame, frame, mask=mask_green)
+#     cv2.putText(res_green, "Green", (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+#
+#     l_h_r = cv2.getTrackbarPos("LH_RED", "GUI")
+#     l_s_r = cv2.getTrackbarPos("LS_RED", "GUI")
+#     l_v_r = cv2.getTrackbarPos("LV_RED", "GUI")
+#     u_h_r = cv2.getTrackbarPos("UH_RED", "GUI")
+#     u_s_r = cv2.getTrackbarPos("US_RED", "GUI")
+#     u_v_r = cv2.getTrackbarPos("UV_RED", "GUI")
+#     l_b_r = np.array([l_h_r, l_s_r, l_v_r])
+#     u_b_r = np.array([u_h_r, u_s_r, u_v_r])
+#     mask_red = cv2.inRange(hsv, l_b_r, u_b_r)
+#     res_red = cv2.bitwise_and(frame, frame, mask=mask_red)
+#     cv2.putText(res_red, "Red", (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+#
+#     l_h_b = cv2.getTrackbarPos("LH_BLUE", "GUI")
+#     l_s_b = cv2.getTrackbarPos("LS_BLUE", "GUI")
+#     l_v_b = cv2.getTrackbarPos("LV_BLUE", "GUI")
+#     u_h_b = cv2.getTrackbarPos("UH_BLUE", "GUI")
+#     u_s_b = cv2.getTrackbarPos("US_BLUE", "GUI")
+#     u_v_b = cv2.getTrackbarPos("UV_BLUE", "GUI")
+#     l_b_b = np.array([l_h_b, l_s_b, l_v_b])
+#     u_b_b = np.array([u_h_b, u_s_b, u_v_b])
+#     mask_blue = cv2.inRange(hsv, l_b_b, u_b_b)
+#     res_blue = cv2.bitwise_and(frame, frame, mask=mask_blue)
+#     cv2.putText(res_blue, "Blue", (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+#     # cv2.imshow("blue", res_blue)
+#
+#     top = np.hstack((res_yellow, res_green))
+#     bottom = np.hstack((res_red, res_blue))
+#     img = np.vstack((top, bottom))
+#     cv2.imshow("Calibration", cv2.resize(img, (0, 0), fx=0.6, fy=0.6))
+#     return l_b_y, u_b_y, l_b_g, u_b_g, l_b_r, u_b_r, l_b_b, u_b_b
+#
 
 
 def find(mask):
--- Track.py
+++ Track.py
@@ -2,67 +2,85 @@ import cv2
 import Kuka
 from pyModbusTCP.client import ModbusClient
 import time
+import threading
+
+# Declare the initial threshold values
+thresholds = ((0.0, 0.0, 0.0), (179.0, 255.0, 255.0), (0.0, 0.0, 0.0), (179.0, 255.0, 255.0), (0.0, 0.0, 0.0),
+              (179.0, 255.0, 255.0), (0.0, 0.0, 0.0), (179.0, 255.0, 255.0))
 
 
-client = ModbusClient("192.168.1.192", 502)
+# Define the calibration function that will run continuously via multithreading
+def calibration():
+    global thresholds
+    while True:
+        thresholds = Kuka.calibrate(window)  # Update the threshold values
+
+
+client = ModbusClient("192.168.1.192", 502)  # Connect to modbus
 while not client.open():
     print("Connecting to Modbus...")
 print("Connected to Modbus!")
-Cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-Kuka.init_GUI()
+Cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)     # Initialize camera
+window = Kuka.init_GUI()                     # Initialize GUI
+calibrate_thread = threading.Thread(target=calibration)  # Declare a thread to run the calibration function
+calibrate_thread.start()                                 # start the thread
 
 while True:
-    client.open()
-    color = 0
-    x = y = 0
+    color, x, y, = 0, 0, 0
+
+    # read from modbus
     yellow = client.read_coils(10)[0]
     green = client.read_coils(11)[0]
     red = client.read_coils(12)[0]
     blue = client.read_coils(13)[0]
 
-    print(yellow, green, red, blue)
+    ret, frame = Cap.read()                         # Read from camers
+    blur = cv2.GaussianBlur(frame, (25, 25), 10)    # Blur the image
+    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)     # make a HSV version of the blurred image
 
-    ret, frame = Cap.read()
-    blur = cv2.GaussianBlur(frame, (25, 25), 10)
+    Kuka.show_cal_screen(hsv, frame, thresholds)    # Show the calibration screen
 
-    lower_yellow, upper_yellow, lower_green, upper_green, lower_red, upper_red, lower_blue, upper_blue = Kuka.calibrate(blur)
-    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-
+    # If yellow variable is True, make a mask from the threshold values and find the position of the object-
+    # draw a circle on the object and write "yellow" by the circle, set color variable to 1 representing yellow
     if yellow:
-        mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+        mask = cv2.inRange(hsv, thresholds[0], thresholds[1])
         x, y = Kuka.find(mask)
         cv2.circle(frame, (x, y), 7, (0, 0, 0), -1)
         cv2.putText(frame, "Yellow", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
         color = 1
 
-    if green and x == 0:
-        mask = cv2.inRange(hsv, lower_green, upper_green)
+    # If green variable is True and no yellow object was found, do the same as above for green, then red, then blue
+    if green and color == 0:
+        mask = cv2.inRange(hsv, thresholds[2], thresholds[3])
         x, y = Kuka.find(mask)
         cv2.circle(frame, (x, y), 7, (0, 0, 0), -1)
         cv2.putText(frame, "Green", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
         color = 2
 
-    if red and x == 0:
-        mask = cv2.inRange(hsv, lower_red, upper_red)
+    if red and color == 0:
+        mask = cv2.inRange(hsv, thresholds[4], thresholds[5])
         x, y = Kuka.find(mask)
         cv2.circle(frame, (x, y), 7, (0, 0, 0), -1)
         cv2.putText(frame, "Red", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
         color = 3
 
-    if blue and x == 0:
-        mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    if blue and color == 0:
+        mask = cv2.inRange(hsv, thresholds[6], thresholds[7])
         x, y = Kuka.find(mask)
         cv2.circle(frame, (x, y), 7, (0, 0, 0), -1)
         cv2.putText(frame, "Blue", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
         color = 4
 
+    # write variables for x- and y-position and color to modbus
     client.write_single_register(32001, x)
     client.write_single_register(32002, y)
     client.write_single_register(32003, color)
+
+    #show video feed
     cv2.imshow("result", frame)
+
+    # if esc key is pressed, break the loop and end the program
     key = cv2.waitKey(5)
-
-
     if key == 27:
         break
 
